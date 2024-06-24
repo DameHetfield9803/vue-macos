ab3a4274143f34ba14433144fb68cb92f1fe36d7
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-06-24 20:17:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <body class="main space-y-6">
+    <h1 class=" text-[3em]">Hello world</h1>
+    <p>This is a test using Vue on Mac</p>
+    <div class="card container flex flex-wrap mx-auto">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, nostrum!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio doloremque ipsam sapiente voluptatum quos dolores
+        temporibus asperiores corporis qui dolorum!</p>
+    </div>
+  </body>
+</template>
+
+<style>
+.card {
+  background-color: #fff5f5;
+  font-size: 15px;
+  color: teal;
+  padding: 8px;
+  border-radius: 7px;
+  text-wrap: pretty;
+  max-width: fit-content;
+  max-height: fit-content;
+}
+
+.main {
+  justify-items: center;
+  text-align: center;
+  background-color: black;
+  color: antiquewhite;
+  padding-top: 12px;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  min-height: 100vh;
+  min-width: 100%;
+}
+</style>
